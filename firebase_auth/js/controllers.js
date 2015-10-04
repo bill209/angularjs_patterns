bbd102b00853cc8813c76e6a4952c12ee1eb820f
bill rowland <bill.rowland@bcbsnc.com>
2015-10-04 12:58:57 -0400

    adding in success response

--- firebase_auth/js/controllers.js
+++ firebase_auth/js/controllers.js
@@ -5,22 +5,25 @@
 		.module('FirebaseApp')
 		.controller('FirebaseCtrl', fbCtrl );
 
-	function fbCtrl(firebaseFactory, $firebaseArray){
+	function fbCtrl(firebaseFactory, $firebaseArray, $timeout){
 		var vm = this;
 		vm.t = {};
 		vm.t.EMAIL = false;
 		vm.t.PWD = false;
+        vm.successfulRegistration = false;
 
 		vm.addUser = addUser;
 
 		function addUser(userData){
 			firebaseFactory.addUser(userData, function(err,user) {
-console.log('userData',userData);
-console.log('err',err);
-console.log('user',user);
-				if(err != false){
-					vm.t.EMAIL = true;
-				}
+				if(err != null){
+                    console.log('error',err,user);
+				} else {
+                    $timeout(function(){
+                        vm.successfulRegistration = true;
+                    });
+                    console.log('success');
+                }
 			});
 		}
 
--- firebase_auth/js/svc.js
+++ firebase_auth/js/svc.js
@@ -1,17 +1,17 @@
-(function() {
+(function () {
 	'use strict';
 
 	angular
 		.module('FirebaseApp')
-		.factory('firebaseFactory',function($firebaseArray){
+		.factory('firebaseFactory', function ($firebaseArray) {
 
-			var ref = new Firebase(myFirebase);
-			var addUser = function(userData, callback){
+            var ref = new Firebase(myFirebase);
+			var addUser = function (userData, callback) {
 				ref.createUser({
 					'email' : userData.email,
 					'password' : userData.password
 				}, callback);
 			};
-			return { addUser : addUser }
+			return { addUser : addUser };
 		});
 })();
